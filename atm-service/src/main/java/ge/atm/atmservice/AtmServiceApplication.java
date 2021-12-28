aa5b1322b7893ae7721f77a3900d12893871c0a0
Vako Beridze <vako.beridze@gmail.com>
2021-12-28 17:28:20 +0400

    create atm emulator project with two submodules

---
+++ atm-service/src/main/java/ge/atm/atmservice/AtmServiceApplication.java
@@ -0,0 +1,13 @@
+package ge.atm.atmservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtmServiceApplication.class, args);
+    }
+
+}
